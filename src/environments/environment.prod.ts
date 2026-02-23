cbb1d42a42116a9a38deb8656bcc2dd2df86e372
pablo10e36366 <pmortega@sudamericano.edu.ec>
2026-02-23 14:28:08 -0500

    fix api railway url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://promanage-backend-production-ec6c.up.railway.app/api',
+  apiUrl: 'https://promanage-backend-production-ec6c.up.railway.app/api'
 };
